78ae241a064715578274e0de00cdc49875666e9e
Sarvarbek Boltaboyev <ssarvar507@gmail.com>
2026-04-08 21:49:24 +0500

    positive or negative number defining programm

---
+++ posOrNeg.dart
@@ -0,0 +1,13 @@
+import 'dart:io';
+
+void main() { 
+    print('Enter your numnber: '); 
+    String? input = stdin.readLineSync(); 
+    int number = int.tryParse(input ?? '') ?? 0; 
+
+    if (number > 0) { 
+        print("Your number is positive."); 
+    } else { 
+        print('Your number is negative.')
+    }
+}
